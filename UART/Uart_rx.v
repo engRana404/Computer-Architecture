1b096c5784f5eeaef89dd09b4996fbd4f47924d6
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2022-12-27 00:55:12 +0200

    Update Uart_rx.v

--- UART/Uart_rx.v
+++ UART/Uart_rx.v
@@ -1,7 +1,7 @@
 //1 start_bit + 8 data_bits + 1 stop_bit
-module Uart_rx(clk,rx_to_fifo,rx_dout);
+module Uart_rx(clk,rx_done,rx_dout);
 input           clk; 
-output reg     rx_to_fifo;
+output reg     rx_done;
 output reg  [7:0]      rx_dout;
 reg  [3:0]      OS_count;//oversampling counter max:15
 reg  [3:0]      data_count; //d0:d8-->8 data bits
@@ -43,7 +43,7 @@ always@(*) begin
     RX_DATA: begin
         if(OS_count == 4'd15) begin
           OS_count=0;
-          rx_dout=rx_dout|(rx_start<<1);
+          rx_dout= rx_start|(rx_dout>>1);
           if(data_count==(NBits-1)) begin    
                 nextstate = STOP;
             end
@@ -60,7 +60,7 @@ always@(*) begin
     
     STOP: begin
         if(OS_count==4'd7) begin
-            rx_to_fifo=1;
+            rx_done=1;
             nextstate = IDLE;
         end
         else begin
