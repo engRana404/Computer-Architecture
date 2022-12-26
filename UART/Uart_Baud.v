9ce7f2556bb9c1a79eb55f132646e80f760a48ae
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2022-12-26 19:43:08 +0200

    add Baud_Rate_Generator_timer

---
+++ UART/Uart_Baud.v
@@ -0,0 +1,40 @@
+
+/*Oversampling by making reciever faster than transmitter by 16 times*/
+module Baud_generator_timer
+#(parameter CLOCK_RATE = 100000000, //f=100MHZ frequency of microcontroller
+    parameter BAUD_RATE = 9600)(
+    input wire clk, 
+    output reg rx_tick, //receiver clock 
+    output reg tx_tick //transmitter clock
+);
+parameter FINAL_VALUE_RX = CLOCK_RATE / (BAUD_RATE * 16); //oversample  
+parameter FINAL_VALUE_TX = CLOCK_RATE / (BAUD_RATE);
+parameter RX_COUNT_WIDTH = $clog2(FINAL_VALUE_RX);//calculate the minimum width required to address a memory of given size
+parameter TX_COUNT_WIDTH = $clog2(FINAL_VALUE_TX);
+
+reg [RX_COUNT_WIDTH - 1:0] rxCount = 0;
+reg [RX_COUNT_WIDTH - 1:0] txCount = 0;
+
+initial begin
+    rx_tick = 1'b0;
+    tx_tick = 1'b0;
+end
+
+always @(posedge clk) begin
+    // rx_clk
+    if (rxCount == FINAL_VALUE_RX[RX_COUNT_WIDTH-1:0]) begin
+        rxCount <= 0;
+        rx_tick <= ~rx_tick;
+    end else begin
+        rxCount <= rxCount + 1'b1;
+    end
+    // tx_clk
+    if (txCount == FINAL_VALUE_TX[RX_COUNT_WIDTH-1:0]) begin
+        txCount <= 0;
+        tx_tick <= ~tx_tick;
+    end else begin
+        txCount <= txCount + 1'b1;
+    end
+end
+
+endmodule
