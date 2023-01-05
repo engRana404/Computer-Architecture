4870d25ab0c44e80454a8be736db89d5489180b0
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2023-01-05 16:27:49 +0200

    Update Uart_rx.v

--- UART/Uart_rx.v
+++ UART/Uart_rx.v
@@ -1,54 +1,92 @@
-`include "uart.v"
+//1 start_bit + 8 data_bits + 1 stop_bit
+module Uart_rx
+#(
+    parameter DBITS=8
+)
+(clk,reset,s_tick,rx_start,rx_done,rx_dout);
+input          clk, s_tick; 
+input          rx_start; 
+input          reset;
+output reg     rx_done;
+output [DBITS-1:0]      rx_dout;
 
-module uart_tx_test();
+reg  [3:0]      OS_count, OS_count_next;//oversampling counter max:15
+reg  [3:0]      data_count, data_count_next; //d0:d8-->8 data bits
+ 
+reg  [DBITS-1:0] ShiftReg, ShiftReg_next;
+reg  [1:0]      state, nextstate;
 
-reg [7:0] data = 0;
-reg clk = 0;
-reg enable = 0;
-
-wire tx_busy;
-wire rdy;
-wire [7:0] rxdata;
-
-wire loopback;
-reg rdy_clr = 0;
-
-uart test_uart(.din(data),
-	       .wr_en(enable),
-	       .clk_50m(clk),
-	       .tx(loopback),
-	       .tx_busy(tx_busy),
-	       .rx(loopback),
-	       .rdy(rdy),
-	       .rdy_clr(rdy_clr),
-	       .dout(rxdata));
-
-initial begin
-	$dumpfile("uart.vcd");
-	$dumpvars(0, uart_tx_test);
-	enable <= 1'b1;
-	#2 enable <= 1'b0;
+//FSM
+parameter      IDLE        = 2'b00;
+parameter      START       = 2'b01;
+parameter      RX_DATA     = 2'b10;
+parameter      STOP        = 2'b11;
+// state to nextstate with clk in this block.
+always@(posedge clk or negedge reset)begin
+    if(!reset) begin
+        state <= IDLE;
+        OS_count<=0;
+        data_count<=0;
+        ShiftReg<=0;
+    end
+    else begin
+        state <= nextstate;
+        OS_count<=OS_count_next;
+        data_count<=data_count_next;
+        ShiftReg<=ShiftReg_next;
+    end
 end
 
-always begin
-	#1 clk = ~clk;
-end
+// nextstate transform
+always@(*) begin
+  nextstate=state;
+  OS_count_next=OS_count;
+  data_count_next=data_count;
+  ShiftReg_next=ShiftReg;
+  rx_done=1'b0;
+    case(state)
+    IDLE: begin
+        if(rx_start==0) begin
+            OS_count_next=0;
+            nextstate = START;
+        end
+    end
+    START: begin
+      if(s_tick) 
 
-always @(posedge rdy) begin
-	#2 rdy_clr <= 1;
-	#2 rdy_clr <= 0;
-	if (rxdata != data) begin
-		$display("FAIL: rx data %x does not match tx %x", rxdata, data);
-		$finish;
-	end else begin
-		if (rxdata == 8'hff) begin
-			$display("SUCCESS: all bytes verified");
-			$finish;
-		end
-		data <= data + 1'b1;
-		enable <= 1'b1;
-		#2 enable <= 1'b0;
-	end
+        if(OS_count==7) //OS_count==7
+        begin
+            OS_count_next=0;
+            data_count_next=0;
+            nextstate = RX_DATA;
+        end
+        else 
+          OS_count_next=OS_count+1; 
+    end
+    RX_DATA: begin
+      
+if(s_tick)
+        if(OS_count == 15) begin
+          OS_count_next=0;
+          ShiftReg_next= {rx_start, ShiftReg[7:1]};
+          if(data_count==(DBITS-1))     
+              nextstate = STOP;
+          else  
+            data_count_next=data_count+1; 
+        end
+        else 
+            OS_count_next=OS_count+1;  
+    end
+    
+    STOP: begin
+        if(OS_count==15) begin
+            rx_done=1'b1;
+            nextstate = IDLE;
+        end
+        else 
+          OS_count_next=OS_count+1;     
+    end
+    endcase
 end
-
+    assign rx_dout=ShiftReg;
 endmodule
