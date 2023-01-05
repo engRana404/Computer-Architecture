3e5fd00fc8d19d172ebca95dd82d40ac75d11541
S0nd0s <104691011+S0nd0s@users.noreply.github.com>
2023-01-05 21:12:18 +0200

    Update baud_rate_gen.v

--- UART/baud_rate_gen.v
+++ UART/baud_rate_gen.v
@@ -2,7 +2,7 @@
  * baud rate generator to divide a 50MHz clock into a 115200 baud
  * rx/tx pair where the rx clcken oversamples by 16x.
  */
-module baud_rate_gen(input wire clk_50m,
+module baud_rate_gen(input wire CLK,
 		     output wire rxclk_en,
 		     output wire txclk_en);
 
@@ -16,14 +16,14 @@ reg [TX_ACC_WIDTH - 1:0] tx_acc = 0;
 assign rxclk_en = (rx_acc == 5'd0);
 assign txclk_en = (tx_acc == 9'd0);
 
-always @(posedge clk_50m) begin
+	always @(posedge CLK) begin
 	if (rx_acc == RX_ACC_MAX[RX_ACC_WIDTH - 1:0])
 		rx_acc <= 0;
 	else
 		rx_acc <= rx_acc + 5'b1;
 end
 
-always @(posedge clk_50m) begin
+	always @(posedge CLK) begin
 	if (tx_acc == TX_ACC_MAX[TX_ACC_WIDTH - 1:0])
 		tx_acc <= 0;
 	else
